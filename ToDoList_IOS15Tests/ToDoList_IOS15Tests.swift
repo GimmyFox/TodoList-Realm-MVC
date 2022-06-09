6432f2cfddd342c1e9ae9a807fd1dbc8bb8675fa
GimmyFox <mguzeev99@gmail.com>
2022-06-09 17:33:03 +0300

    Initial Commit

---
+++ ToDoList_IOS15Tests/ToDoList_IOS15Tests.swift
@@ -0,0 +1,33 @@
+//
+//  ToDoList_IOS15Tests.swift
+//  ToDoList_IOS15Tests
+//
+//  Created by Maksim Guzeev on 09.06.2022.
+//
+
+import XCTest
+@testable import ToDoList_IOS15
+
+class ToDoList_IOS15Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
